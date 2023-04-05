03165fbd899ed099a5a9c1c3b5e3edd8eeab65cf
Жуков Александр Сергеевич <ASergeevicZhukov@sberbank.ru>
2023-04-05 18:21:23 +0300

    add taks3

--- Python/python_generation/03datetime/module5/task3.py
+++ Python/python_generation/03datetime/module5/task3.py
@@ -1,15 +1,35 @@
-from datetime import datetime, timedelta
+from datetime import datetime, time
 
-schedule = {
-    0: {'start': timedelta(hours=9), 'end': timedelta(hours=21)},
-    1: {'start': timedelta(hours=9), 'end': timedelta(hours=21)},
-    2: {'start': timedelta(hours=9), 'end': timedelta(hours=21)},
-    3: {'start': timedelta(hours=9), 'end': timedelta(hours=21)},
-    4: {'start': timedelta(hours=9), 'end': timedelta(hours=21)},
-    5: {'start': timedelta(hours=10), 'end': timedelta(hours=18)},
-    6: {'start': timedelta(hours=10), 'end': timedelta(hours=18)}
-    }
+schedule = {}
+for i in range(7):
+    open_time = time(9, 0) if i < 5 else time(10, 0)
+    close_time = time(21, 0) if i < 5 else time(18, 0)
+    schedule[i] = {"open": open_time, "close": close_time}
 
 pattern = "%d.%m.%Y %H:%M"
 
 input_date = datetime.strptime(input(), pattern)
+weekday = input_date.weekday()
+combined_date = datetime.combine(datetime.now().date(), input_date.time())
+
+time_to_open = (combined_date - datetime.combine(datetime.today(), schedule[weekday]['open'])).total_seconds()
+time_to_close = (datetime.combine(datetime.today(), schedule[weekday]['close']) - combined_date).total_seconds()
+
+if time_to_open > 0 and time_to_close > 0:
+    print(int(time_to_close // 60))
+
+else:
+    print("Магазин не работает")
+
+
+# from datetime import datetime, timedelta
+#
+# dt = datetime.strptime(input(), '%d.%m.%Y %H:%M')
+# td = timedelta(hours=dt.hour, minutes=dt.minute)
+#
+# if dt.weekday() < 5 and timedelta(hours=9) <= td < timedelta(hours=21):
+#     print(int((timedelta(hours=21) - td).total_seconds() // 60))
+# elif dt.weekday() > 4 and timedelta(hours=10) <= td < timedelta(hours=18):
+#     print(int((timedelta(hours=18) - td).total_seconds() // 60))
+# else:
+#     print('Магазин не работает')
--- Python/python_generation/03datetime/module5/task7.py
+++ Python/python_generation/03datetime/module5/task7.py
@@ -5,8 +5,10 @@ pattern = "%d.%m.%Y"
 day_month_pattern = "%d.%m"
 seven_days = timedelta(days=7)
 
-current_date = datetime.strptime(input(), day_month_pattern)
+current_date = datetime.strptime(input(), pattern)
 employees = [input().split() for _ in range(int(input()))]
 
-nearest_birhday_employees = [emp for emp in employees
-                             if datetime.strptime(emp[2], day_month_pattern) + seven_days ]
+print(current_date + seven_days)
+print(datetime.strptime(employees[0][2], day_month_pattern))
+# nearest_birhday_employees = [emp for emp in employees
+#                              if current_date <= datetime.timedelta(days=<= current_date + seven_days ]
